a8e38887a9f561bbd1357f35021738bcb38ddd2e
Raphaelpeluso <raphaelpeluso@gmail.com>
2023-09-20 22:13:04 -0300

    Url/Preço

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,65 @@
-    //pegar os valores do formulario
-    let nome = document.querySelector("#nome")
-    
-    const campoQtde = document.querySelector("#qtde")
-    const output = document.querySelector("output")
-    const campoTipo = document.querySelector("#tipo")
-    const campoLayoutSim =  document.querySelector("#layout_sim")
-    const campoLayoutNao =  document.querySelector("#layout_nao")
-    const campoVisu =  document.querySelector("#visu")
-    const campoPrazo = document.querySelector("#prazo")
+//pegar os valores do formulario
+let nome = document.querySelector("#nome")
 
-    campoPrazo.addEventListener("input", function(){
-        const labelprazo = document.querySelector("label[for='prazo']")
-        labelprazo.innerHTML = `Prazo: ${campoPrazo.value} semanas`
-        calcular()
-    })
+const campoQtde = document.querySelector("#qtde")
+const output = document.querySelector("output")
+const campoTipo = document.querySelector("#tipo")
+const campoLayoutSim = document.querySelector("#layout_sim")
+const campoLayoutNao = document.querySelector("#layout_nao")
+const campoVisu = document.querySelector("#visu")
+const campoPrazo = document.querySelector("#prazo")
 
-    campoQtde.addEventListener("change", calcular)
-    campoTipo.addEventListener("change", calcular)
-    campoLayoutSim.addEventListener("change", calcular)
-    campoLayoutNao.addEventListener("change", calcular)
-    campoVisu.addEventListener("change", calcular)
-    
-    //calcular o preço
-    function calcular(){
-      //R$100 por som
-      let qtde = campoQtde.value
-      let valor = qtde * 100
-      
-      //mudar para switch
-       switch (campoTipo.value){
-            case "1":
-                valor += 100
-                break
-            case "2":
-                valor += 100
-                break
-            case "3":
-                valor += 100
-                break
-            case "4":
-                valor += 100
-                break
-        }
-        
-        if(campoLayoutSim.Checked){
-            valor += qtde.value * 1500  
-        }
+campoPrazo.addEventListener("input", function () {
+    const labelprazo = document.querySelector("label[for='prazo']")
+    labelprazo.innerHTML = `Prazo: ${campoPrazo.value} semanas`
+    calcular()
+})
 
-        if(campoVisu.Checked){
-            valor *= 1.1  
-        }
+campoQtde.addEventListener("change", calcular)
+campoTipo.addEventListener("change", calcular)
+campoLayoutSim.addEventListener("change", calcular)
+campoLayoutNao.addEventListener("change", calcular)
+campoVisu.addEventListener("change", calcular)
 
-        let taxaDeUrgencia = 1 - campoPrazo.value * 0.05
-        valor *= 1 + taxaDeUrgencia
+//calcular o preço
+function calcular() {
+    //R$100 por som
+    let qtde = campoQtde.value
+    let valor = qtde * 100
 
-      output.innerText = `R$ ${valor}`
-    
+    console.log(valor)
+
+    //mudar para switch
+    switch (campoTipo.value) {
+        case "1":
+            valor += 100
+            break
+        case "2":
+            valor += 100
+            break
+        case "3":
+            valor += 100
+            break
+        case "4":
+            valor += 100
+            break
     }
+    console.log(valor)
+    
+    if (campoLayoutSim.checked) {
+        valor += qtde * 1500
+    }
+    console.log(valor)
+    
+    if (campoVisu.checked) {
+        valor *= 1.1
+    }
+    console.log(valor)
+
+    let taxaDeUrgencia = 1 - campoPrazo.value * 0.05
+    valor *= 1 + taxaDeUrgencia
+
+    output.innerText = `R$ ${valor.toFixed(2)}`
+
+}
     //mostrar o preço
